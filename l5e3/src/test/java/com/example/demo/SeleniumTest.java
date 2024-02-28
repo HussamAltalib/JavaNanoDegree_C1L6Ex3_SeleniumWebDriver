094b56e50f4263841f9ace34f7cd3329d82cd770
HussamAltalib <hussamaltalib@outlook.com>
2024-02-28 14:22:15 +0300

    course solution

--- l5e3/src/test/java/com/example/demo/SeleniumTest.java
+++ l5e3/src/test/java/com/example/demo/SeleniumTest.java
@@ -6,17 +6,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 public class SeleniumTest {
     public static void main(String[] args) throws InterruptedException {
-        WebDriverManager.chromedriver().browserVersion("122.0.6261.70").setup();
+        //start the driver, open chrome to our target url
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:8080/animal");
-        WebElement inputField = driver.findElement(By.id ("animalText"));
-        inputField.sendKeys("CatSel");
-        WebElement adjectiveField = driver.findElement(By.id ("adjective"));
-        adjectiveField.sendKeys("MewoooXX");
-        adjectiveField.submit();
-        Thread.sleep(5000);
+
+
+        //find the fields we want by id and fill them in
+        WebElement inputField = driver.findElement(By.id("animalText"));
+        inputField.sendKeys("Manatee");
+
+        inputField = driver.findElement(By.id("adjective"));
+        inputField.sendKeys("Whirling");
+
+        List<WebElement> trainingResults = driver.findElements(By.className("trainingMessage"));
+
+        // The field-values don’t clear on submit for our simple app, so just submit it 5 times
+        // However, the elements gets removed from the DOM structure after each submit.
+        for(int i = 0; i < 5; i++) {
+            // We are re-assigning the inputField because this element gets removed from the DOM structure after each iteration.
+            // Otherwise, you'll get org.openqa.selenium.StaleElementReferenceException at runtime.
+            inputField = driver.findElement(By.id("adjective"));
+            inputField.submit();
+
+            System.out.println("trainingResults.size() = " + trainingResults.size());
+        }
+
+        // then get the element by the class conclusionMessage and print it
+        WebElement conclusionResult = driver.findElement(By.className("conclusionMessage"));
+        System.out.println("conclusionResult.getText() = " + conclusionResult.getText());
+
+        Thread.sleep(10000);
         driver.quit();
     }
 }
